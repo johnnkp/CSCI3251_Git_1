0eb28f32b2135d88ad109b7f758ce552512b6a08
johnnkp <kpng0@cse.cuhk.edu.hk>
2023-03-31 00:49:23 +0800

    add test case

--- test/test.py
+++ test/test.py
@@ -3,3 +3,18 @@ from lab7 import check_type
 
 def test_check_type():
     assert check_type() == False
+    assert check_type('test') == False
+
+    try:
+        check_type(123)
+    except TypeError:
+        assert True
+    else:
+        assert False
+
+    myname = "john"
+    myID = 123456789
+    assert check_type(myname, myID) == True
+
+    myID2 = "123456789"
+    assert check_type(myname, myID2) == True
